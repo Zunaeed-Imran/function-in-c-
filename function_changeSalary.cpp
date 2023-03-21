e09ed113edb9260666fbad64ae395c202db4e2d4
MD Zunaeed Imran <124905281+Zunaeed-Imran@users.noreply.github.com>
2023-03-21 18:58:03 +0600

    Add files via upload

---
+++ function_changeSalary.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+void Salary_Change(int s){
+  s=30000;
+  cout<<"value inside function"<<s <<endl;
+}
+
+int main(){
+  int sal=27000;
+  Salary_Change(sal);
+  cout<<"Value inside main "<< sal <<endl;
+  return 0;
+}
